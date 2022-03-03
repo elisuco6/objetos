d219cd63063f9b5f15b44d7ed6a427b191256b83
elisuco6 <elisalexandru32@gmail.com>
2022-03-03 19:49:25 +0100

    commit 5

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -8,39 +8,49 @@ import java.util.Scanner;
 /**
  * Pre: ---
  * Post: Implementa un método informacionVideojuegos() que devuelve un ArrayList<String> que
- *       agrupe todas las plataformas para las que hay videojuegos en el fichero entregado. Es
- *       decir, se debe almacenar UNA SOLA VEZ, cada una de las plataformas que aparecen en
- *       el fichero .csv.
+ * agrupe todas las plataformas para las que hay videojuegos en el fichero entregado. Es
+ * decir, se debe almacenar UNA SOLA VEZ, cada una de las plataformas que aparecen en
+ * el fichero .csv.
  */
 public class Ejercicio3 {
+
+    /**
+     * Pre: ---
+     * Post: En el main pasamos por parametro la ruta del fichero y llamamos al metodo informacion
+     */
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         String fichero = "C:/Users/elisa/OneDrive/Escritorio/ventasVideojuegos.csv";
         informacionVideojuegos(fichero);
     }
 
+    /**
+     * Pre: ---
+     * Post: creamos un array donde se va guardar el 2 split empezando por 0 es decir el nombre de la consola
+     * posteriormente
+     */
     public static void informacionVideojuegos(String fichero) {
         ArrayList<String> consolas = new ArrayList<String>();
+        ArrayList<String> consolascopia = new ArrayList<String>();
         File file = new File(fichero);
         boolean linea1 = true;
         try {
             Scanner f = new Scanner(file);
-            int consolasdistintas = 0;
+            int contarconsolas = 0;
             while (f.hasNextLine()) {
                 String linea = f.nextLine();
+                String duplicado = "";
                 if (linea1) {
                     linea1 = false;
                 } else {
                     String[] lineaSep = linea.split(",");
-                    for (int i = 0;i<lineaSep.length;i++){
-                        if (lineaSep[2].contentEquals(lineaSep[2])){
-                            consolas.add(lineaSep[2]);
-                        }
-                    }
-                    System.out.println(consolas);
+                    consolas.add(lineaSep[2]);
+
                 }
             }
-        }catch(FileNotFoundException e) {
+            System.out.println(consolas);
+        } catch (
+                FileNotFoundException e) {
             System.out.println(e);
         }
     }
